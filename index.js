76df3fb62476f2145f58d3643a7a1af72d0bb3fa
ahmad-arnika <ahmad.sharif.abc@gmail.com>
2024-01-22 11:08:29 +0330

    css class

--- index.js
+++ index.js
@@ -63,288 +63,66 @@ Highcharts.chart('fig_container', {
         type: 'pie'
     },
     title: {
-        text: 'Browser market shares. January, 2022',
-        align: 'left'
+        text: 'تحلیل بخش بندی مشتری'
+    },
+    tooltip: {
+        valueSuffix: '%'
     },
     subtitle: {
-        text: 'Click the slices to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>',
-        align: 'left'
+        text:
+        'Source:<a href="https://www.mdpi.com/2072-6643/11/3/684/htm" target="_default">MDPI</a>'
     },
-
-    accessibility: {
-        announceNewData: {
-            enabled: true
-        },
-        point: {
-            valueSuffix: '%'
-        }
-    },
-
     plotOptions: {
         series: {
-            borderRadius: 5,
+            allowPointSelect: true,
+            cursor: 'pointer',
             dataLabels: [{
                 enabled: true,
-                distance: 15,
-                format: '{point.name}'
+                distance: 20
             }, {
                 enabled: true,
-                distance: '-30%',
-                filter: {
-                    property: 'percentage',
-                    operator: '>',
-                    value: 5
-                },
-                format: '{point.y:.1f}%',
+                distance: -40,
+                format: '{point.percentage:.1f}%',
                 style: {
-                    fontSize: '0.9em',
-                    textOutline: 'none'
+                    fontSize: '1.2em',
+                    textOutline: 'none',
+                    opacity: 0.7
+                },
+                filter: {
+                    operator: '>',
+                    property: 'percentage',
+                    value: 10
                 }
             }]
         }
     },
-
-    tooltip: {
-        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-    },
-
     series: [
         {
-            name: 'Browsers',
+            name: 'Percentage',
             colorByPoint: true,
             data: [
                 {
-                    name: 'Chrome',
-                    y: 61.04,
-                    drilldown: 'Chrome'
+                    name: 'Champions',
+                    y: 55.02
                 },
                 {
-                    name: 'Safari',
-                    y: 9.47,
-                    drilldown: 'Safari'
+                    name: 'Loyal Customers',
+                    y: 26.71
                 },
                 {
-                    name: 'Edge',
-                    y: 9.32,
-                    drilldown: 'Edge'
+                    name: 'Potential Loyalists',
+                    y: 1.09
                 },
                 {
-                    name: 'Firefox',
-                    y: 8.15,
-                    drilldown: 'Firefox'
+                    name: 'New Customers',
+                    y: 15.5
                 },
                 {
-                    name: 'Other',
-                    y: 11.02,
-                    drilldown: null
+                    name: 'Promising',
+                    y: 1.68
                 }
             ]
         }
-    ],
-    drilldown: {
-        series: [
-            {
-                name: 'Chrome',
-                id: 'Chrome',
-                data: [
-                    [
-                        'v97.0',
-                        36.89
-                    ],
-                    [
-                        'v96.0',
-                        18.16
-                    ],
-                    [
-                        'v95.0',
-                        0.54
-                    ],
-                    [
-                        'v94.0',
-                        0.7
-                    ],
-                    [
-                        'v93.0',
-                        0.8
-                    ],
-                    [
-                        'v92.0',
-                        0.41
-                    ],
-                    [
-                        'v91.0',
-                        0.31
-                    ],
-                    [
-                        'v90.0',
-                        0.13
-                    ],
-                    [
-                        'v89.0',
-                        0.14
-                    ],
-                    [
-                        'v88.0',
-                        0.1
-                    ],
-                    [
-                        'v87.0',
-                        0.35
-                    ],
-                    [
-                        'v86.0',
-                        0.17
-                    ],
-                    [
-                        'v85.0',
-                        0.18
-                    ],
-                    [
-                        'v84.0',
-                        0.17
-                    ],
-                    [
-                        'v83.0',
-                        0.21
-                    ],
-                    [
-                        'v81.0',
-                        0.1
-                    ],
-                    [
-                        'v80.0',
-                        0.16
-                    ],
-                    [
-                        'v79.0',
-                        0.43
-                    ],
-                    [
-                        'v78.0',
-                        0.11
-                    ],
-                    [
-                        'v76.0',
-                        0.16
-                    ],
-                    [
-                        'v75.0',
-                        0.15
-                    ],
-                    [
-                        'v72.0',
-                        0.14
-                    ],
-                    [
-                        'v70.0',
-                        0.11
-                    ],
-                    [
-                        'v69.0',
-                        0.13
-                    ],
-                    [
-                        'v56.0',
-                        0.12
-                    ],
-                    [
-                        'v49.0',
-                        0.17
-                    ]
-                ]
-            },
-            {
-                name: 'Safari',
-                id: 'Safari',
-                data: [
-                    [
-                        'v15.3',
-                        0.1
-                    ],
-                    [
-                        'v15.2',
-                        2.01
-                    ],
-                    [
-                        'v15.1',
-                        2.29
-                    ],
-                    [
-                        'v15.0',
-                        0.49
-                    ],
-                    [
-                        'v14.1',
-                        2.48
-                    ],
-                    [
-                        'v14.0',
-                        0.64
-                    ],
-                    [
-                        'v13.1',
-                        1.17
-                    ],
-                    [
-                        'v13.0',
-                        0.13
-                    ],
-                    [
-                        'v12.1',
-                        0.16
-                    ]
-                ]
-            },
-            {
-                name: 'Edge',
-                id: 'Edge',
-                data: [
-                    [
-                        'v97',
-                        6.62
-                    ],
-                    [
-                        'v96',
-                        2.55
-                    ],
-                    [
-                        'v95',
-                        0.15
-                    ]
-                ]
-            },
-            {
-                name: 'Firefox',
-                id: 'Firefox',
-                data: [
-                    [
-                        'v96.0',
-                        4.17
-                    ],
-                    [
-                        'v95.0',
-                        3.33
-                    ],
-                    [
-                        'v94.0',
-                        0.11
-                    ],
-                    [
-                        'v91.0',
-                        0.23
-                    ],
-                    [
-                        'v78.0',
-                        0.16
-                    ],
-                    [
-                        'v52.0',
-                        0.15
-                    ]
-                ]
-            }
-        ]
-    }
+    ]
 });
 
